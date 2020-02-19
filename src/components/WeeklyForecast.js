567113def1de91394e0b85d7fc77bcb05d1d83fb
Erez Poliak <poliakerez@gmail.com>
2020-02-19 13:17:15 +0200

    WeeklyForecast Features

--- src/components/WeeklyForecast.js
+++ src/components/WeeklyForecast.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import styled from 'styled-components';
 
 const WeeklyForecast = () => {
+    const [data,setData] = useState([]);
+    const iconUrl = 'https://www.accuweather.com/images/weathericons/';
+    useEffect(() =>{
+        const fetchData = async () =>{
+            let url = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/215854?apikey=Zfo3zMIGUFpf44SjmscYCEAFZRoCbLY8';
+            let fetched = await fetch(url);
+            let jsoned = await fetched.json();
+            setData(jsoned.DailyForecasts);
+        }
+        fetchData();
+    },[data])
+    const getDayName = (dateString) =>{
+        const date = new Date(dateString);
+        const day = date.getDay();
+        if(day === 0) return 'Sunday';
+        if(day === 1) return 'Monday';
+        if(day === 2) return 'Tuesday';
+        if(day === 3) return 'Wednsday';
+        if(day === 4) return 'Thursday';
+        if(day === 5) return 'Friday';
+        if(day === 6) return 'Saturday';
+    }
     return(
         <Container>
-            Weekly Forecast
-            <TempDiv></TempDiv>
+            {data.map(i =>{
+                return <DayDiv>
+                    <div>{getDayName(i.Date)}</div>
+                    <WeatherIcon src = {`${iconUrl}${i.Day.Icon}.svg`}></WeatherIcon>
+                    <div>{`${i.Temperature.Minimum.Value} ${i.Temperature.Maximum.Value}`}</div>
+                </DayDiv>
+            })}
         </Container>
     )
 }
@@ -15,9 +42,19 @@ export default WeeklyForecast;
 const Container = styled.div`
     overflow: scroll;
     border: 1px solid black;
+    display: grid;
+    grid-template-rows: repeat(5, 35%);
 `;
 
-const TempDiv = styled.div`
-    height: 1000px;
+const DayDiv = styled.div`
+    display: flex;
+    justify-content: space-around;
 `;
+
+const WeatherIcon = styled.img`
+    height: 50%;
+    width: 50%;
+`;
+
+
 
