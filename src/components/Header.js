493596bea4401be6c034a8fa65fe78552830afff
erezpoliak <poliakerez@gmail.com>
2020-04-24 18:32:57 +0300

    fixed temp at header responding to temp toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import styled from "styled-components";
 import AutoCompleteSearch from "./AutoCompleteSearch";
 import TempToggle from "./TempToggle";
@@ -6,17 +6,33 @@ import { Forecast_Context } from "./Context";
 import { Link } from "react-router-dom";
 import { ChartBar } from "@styled-icons/fa-regular/ChartBar";
 import { Info } from "@styled-icons/octicons/Info";
+import * as Utility from "./Utility";
 
 const Header = () => {
-  const { currentData } = useContext(Forecast_Context);
+  const { currentData, method } = useContext(Forecast_Context);
+  const [currentTemp, set_currentTemp] = useState({ method: "F", temp: null });
   const description =
     currentData && currentData.weather && currentData.weather.description;
-  const current_temp = Math.round(currentData.temp);
+
+  useEffect(() => {
+    const new_current = { method: "F", temp: currentData.temp };
+    set_currentTemp(new_current);
+  }, [currentData.temp, set_currentTemp]);
+
+  if (method !== currentTemp.method) {
+    const converted_temp = Utility.convertTemp(
+      currentTemp.temp,
+      currentTemp.method
+    );
+    const new_temp = { method: method, temp: converted_temp };
+    set_currentTemp(new_temp);
+  }
+
   return (
     <Wrapper>
       <City>{currentData.city_name}</City>
       <Summary>{description}</Summary>
-      <Temprature>{`${current_temp}°`}</Temprature>
+      <Temprature>{`${Math.round(currentTemp.temp)}°`}</Temprature>
     </Wrapper>
   );
 };
